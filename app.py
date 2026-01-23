f18dd776d90c6b1db199e36789fb513091f7a372
Om Pandey <omp427747@gmail.com>
2026-01-24 01:31:15 +0530

    update

--- app.py
+++ app.py
@@ -9,44 +9,35 @@ from tensorflow.keras.saving import register_keras_serializable
 # --- 1. MODEL LOADING ---
 
 
-@register_keras_serializable()
-class CustomSequential(Sequential):
-    pass
+def create_model():
+    model = Sequential()
 
+    model.add(Conv2D(32, kernel_size=(3, 3),
+              activation='relu', input_shape=(48, 48, 1)))
+    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
 
-with open("facialemotionmodel.json", "r") as json_file:
-    model_json = json_file.read()
+    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
 
-model = model_from_json(model_json, custom_objects={
-                        "Sequential": CustomSequential})
-model.load_weights("facialemotionmodel.h5")
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-EMOTIONS = ["Angry", "Disgust", "Fear", "Happy", "Sad", "Surprise", "Neutral"]
+    model.add(Flatten())
+    model.add(Dense(1024, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(7, activation='softmax'))
 
+    return model
 # --- 2. STABILIZED VIDEO CLASS ---
 
 
-class EmotionTransformer(VideoTransformerBase):  # Using TransformerBase
-    def transform(self, frame):  # Changed 'recv' to 'transform'
-        img = frame.to_ndarray(format="bgr24")
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+model = create_model()
+model.load_weights("facialemotionmodel.h5")
 
-        for (x, y, w, h) in faces:
-            roi_gray = gray[y:y + h, x:x + w]
-            roi_gray = cv2.resize(roi_gray, (48, 48),
-                                  interpolation=cv2.INTER_AREA)
-            roi = roi_gray.astype('float') / 255.0
-            roi = np.expand_dims(roi, axis=0)
-
-            prediction = model.predict(roi, verbose=0)[0]
-            label = EMOTIONS[np.argmax(prediction)]
-
-            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            cv2.putText(img, label, (x, y - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
-
-        return img  # Simply return the array
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+EMOTIONS = ["Angry", "Disgust", "Fear", "Happy", "Sad", "Surprise", "Neutral"]
 
 
 # --- 3. UI ---
